1081f509cfb6fcd749ce91ef02de2b1588120286
SergiEZ <klsergi@gmail.com>
2022-02-01 20:32:26 +0100

    20:32

---
+++ primerjs.js
@@ -0,0 +1,13 @@
+/*let h = "Hola mundo";
+console.log(h);
+let x = 10;
+console.log("x");
+console.log(x);
+console.log(x+2);
+let texto1 = "Hola soy Pepe y tengo ";
+let edad = 18;
+let textoPresentacion = texto1 + edad + " años";
+console.log(textoPresentacion); */
+const rl = require("readline-sync");
+let name = rl.question("Dime tu nombre: ");
+console.log("Tu nombre es: " + name);
