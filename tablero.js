015606515975bf405de6d7565a8233e67a2314d5
SergiEZ <klsergi@gmail.com>
2022-02-07 21:13:59 +0100

    07022022

--- tablero.js
+++ tablero.js
@@ -4,7 +4,7 @@ for(let i = 0; i < 4; i++){
 
     for(let j = 0; j < 8; j++){
         if (j % 2 === 0) process.stdout.write(B + "\t");
-        else process.stdout.write( + "\t");
+        else process.stdout.write(N + "\t");
     }
     console.log("\n");
     for(let j = 0; j < 8; j++){
