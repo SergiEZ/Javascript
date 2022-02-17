c6533870d92d9c31937eb017e6d1940cdbe88125
SergiEZ <klsergi@gmail.com>
2022-02-17 21:43:47 +0100

    17022022

--- funciones.js
+++ funciones.js
@@ -1,3 +1,4 @@
+/*
 let rojo = 200;
 let verde = 45;
 let azul = 187;
@@ -27,3 +28,78 @@ function conversorBytes(b){
 console.log(canalDominante(rojo, verde, azul));
 console.log(colorAleatorio());
 console.log(conversorBytes(2048));
+*/
+/*
+//apellido 1, apellido2, nombre
+function getName(n, a1, a2){
+    return `${a1}, ${a2}, ${n}`;
+}
+console.log(getName("Pepe", "Martinez", "Garcia"));
+
+//Dos números devuelve el max en absoulto
+function maxAbs(n1, n2){
+    if(Math.abs(n1) > Math.abs(n2)) return n1;
+    else return n2;
+}
+console.log(maxAbs(-100, 50));
+
+//construirPatron con n y caracter -> cadena con n caracteres
+function construirPatron(n, c){
+    let cadena = "";
+    for(let i = 0; i < n; i++){
+        cadena += c;
+    }
+    return cadena;
+}
+console.log(construirPatron(5, "*"));
+//triangulo
+function construirPatron(n, c) {
+    let cadena = "";
+    let fila = "";
+    let aux = 1;
+    for (let i = n; i > 0; i--) {
+        for (let j = 0; j < aux; j++) {
+            fila += c;
+        }
+        aux++;
+        cadena += "\n" + fila;
+        fila = "";
+    }
+    return cadena;
+}
+console.log(construirPatron(6, "*"));
+
+//Pasar array de n elementos -> devuelve el array inverso
+function invertirArray(lista){
+    let aux = [];
+    for(let i = 0; i < lista.length; i++){
+        aux[i] = lista[lista.length-1-i];
+    }
+    return aux;
+}
+console.log(invertirArray([568, "coche", "casa", 78, "esponja"]));
+*/
+const rl = require("readline-sync");
+function menu(opciones) {
+    let opt = "";
+    do {
+        for (let i = 0; i < opciones.length; i++) {
+            console.log(`\t${i + 1}.- ${opciones[i]}`);
+        }
+        opt = rl.question("Elige opción: ");
+    } while(isNaN(opt) || Number(opt) < 1 || Number(opt) > opciones.length)
+    return Number(opt);
+}
+
+function menuHTML(opciones){
+    let menu = "<ol>\n"
+    for(let i = 0; i < opciones.length; i++){
+        menu += "\t<li>" + opciones[i] + "</li>\n";
+    }
+    menu += "</ol>"
+    return menu;
+}
+
+let opciones = ["Media", "Suma", "Invertir", "Salir"];
+//menu(opciones);
+console.log(menuHTML(opciones));
--- practica4.js
+++ practica4.js
@@ -1,21 +1,71 @@
-let cadena = "()4*(2/4))(";
+/*
+function comprovaParentesis(cadena) {
+    let parentesis = [];
+    let a = "(";
+    let c = ")";
+    let error = "e";
+    let res;
+
+    for (let i = 0; i < cadena.length; i++) {
+        if (cadena[i] == c && cadena[i - 1] == a)
+            parentesis.push(error);
+        if (cadena[i] == c && parentesis[parentesis.length - 1] != a)
+            parentesis.push(error);
+        if (cadena[i] == c && parentesis[parentesis.length - 1] == a)
+            parentesis.pop();
+        if (cadena[i] == a)
+            parentesis.push(cadena[i]);
+    }
+
+    if (parentesis.length != 0) res = " No está bien expresado.";
+    else res = " Está expresado correctamente.";
+
+    return res;
+}
+
+let expr = "(a+(b/c)-d*(g/h))+(f/s)";
+console.log(expr + comprovaParentesis(expr));
+*/
 let parentesis = [];
 let a = "(";
 let c = ")";
+let error = "e";
+let res;
+function comprovaParentesis(cadena) {
 
-for(let i = 0; i < cadena.length; i++){
-    if(cadena[i] == a) parentesis.push(cadena[i]);
-    if(cadena[i] == c && parentesis[parentesis.length - 1] == a) parentesis.pop();
+
+    for (let i = 0; i < cadena.length; i++) {
+        comprovaBuit();
+        comprovaCorrecte();
+        netejaObert();
+        comprovaObert();
+    }
+
+    if (parentesis.length != 0) res = " No está bien expresado.";
+    else res = " Está expresado correctamente.";
+
+    return res;
 }
 
-console.log(parentesis);
-if(parentesis[0] == a) error();
-else correcto();
-
-function error(){
-    console.log(cadena + " No está bien expresado.");
-} 
-
-function correcto(){
-    console.log(cadena + " Está expresado correctamente.");
+function comprovaBuit() {
+    if (cadena[i] == c && cadena[i - 1] == a)
+        parentesis.push(error);
 }
+
+function comprovaCorrecte() {
+    if (cadena[i] == c && parentesis[parentesis.length - 1] != a)
+        parentesis.push(error);
+}
+
+function netejaObert() {
+    if (cadena[i] == c && parentesis[parentesis.length - 1] == a)
+        parentesis.pop();
+}
+
+function comprovaObert() {
+    if (cadena[i] == a)
+        parentesis.push(cadena[i]);
+}
+
+let expr = "(a+(b/c)-d*(g/h))+(f/s)";
+console.log(expr + comprovaParentesis(expr));
